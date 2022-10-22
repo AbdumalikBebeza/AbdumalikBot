d8c210311f2845df6804b8076e470ae4b7b7c21a
AbdumalikBebeza <sultan.bebeza@gmail.com>
2022-10-22 15:26:44 +0600

    hw4

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from handlers import callback, client, extra, FSM_admin_mentors
+from handlers import callback, client, extra, FSM_admin_mentors, admin
 from aiogram.utils import executor
-
+from database.bot_db import sql_create
 from config import bot, dp
 import logging
+
+
+async def on_startup(_):
+    sql_create()
+
+
+admin.register_message_admin(dp)
 client.register_client_handler(dp)
 callback.register_handler_callback(dp)
 FSM_admin_mentors.register_handlers_fsm_anketa(dp)
 extra.register_handlers_extra(dp)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
